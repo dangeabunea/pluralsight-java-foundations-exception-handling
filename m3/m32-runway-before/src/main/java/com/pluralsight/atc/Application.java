603192ad58791b7e3081759bf88243972be8995e
Dan Geabunea <dangeabunea@hotmail.com>
2025-11-24 12:34:12 +0200

    Improved main methof of m03-02 demo

--- m3/m32-runway-before/src/main/java/com/pluralsight/atc/Application.java
+++ m3/m32-runway-before/src/main/java/com/pluralsight/atc/Application.java
@@ -4,19 +4,14 @@ public class Application {
     public static void main(String[] args) {
         RunwayManager runwayManager = new RunwayManager("LF1");
 
-        // Unsupported operation -> closed
-        //runwayManager.setClosed(true);
-        //runwayManager.requestTakeoff("AF123");
+        // Simulate runway si closed
+        runwayManager.setClosed(true);
 
-        // Unsupported operation -> occupied
-        //runwayManager.requestTakeoff("AF123");
-        //runwayManager.requestTakeoff("AF124");
-
-//        try {
-//            runwayManager.requestTakeoff("AF123");
-//            runwayManager.requestTakeoff("AF124");
-//        } catch (UnsupportedOperationException e){
-//
-//        }
+        try {
+            runwayManager.requestTakeoff("AF123");
+            runwayManager.requestTakeoff("AF124");
+        } catch (UnsupportedOperationException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
