462c7be98b4640e808ef6819bf6fa5a9b57e2dab
Dan Geabunea <dangeabunea@hotmail.com>
2025-11-24 12:01:03 +0200

    Added more code to first demo of module 3

--- m3/m31-weather-before/src/main/java/com/pluralsight/atc/Application.java
+++ m3/m31-weather-before/src/main/java/com/pluralsight/atc/Application.java
@@ -3,5 +3,7 @@ package com.pluralsight.atc;
 public class Application {
     public static void main(String[] args) {
         var weatherService = new AtcWeatherService();
+        String sectorName = "EAST";
+        String eastWeatherReport = null;
     }
 }
