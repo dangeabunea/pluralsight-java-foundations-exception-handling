2d5e40d4f5aedd0c24cd73233e6aabe3ffb33dc0
Dan Geabunea <dangeabunea@hotmail.com>
2025-11-29 19:17:10 +0200

    Made AircraftPosition more readable

--- m5/m51-lambdas-before/src/main/java/com/pluralsight/atc/AircraftPosition.java
+++ m5/m51-lambdas-before/src/main/java/com/pluralsight/atc/AircraftPosition.java
@@ -1,6 +1,9 @@
 package com.pluralsight.atc;
 
-public record AircraftPosition(String callsign, int altitude, double latitude, double longitude) {
+public record AircraftPosition(String callsign,
+                               int altitude,
+                               double latitude,
+                               double longitude) {
     @Override
     public String toString() {
         return String.format("%s at FL%03d (%.4f°, %.4f°)",
